847e9dafb1cd21dba756258722d053f77857d120
unknown <darg.medhed@gmail.com>
2020-10-08 16:24:20 +0100

    better exports

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
-module.exports = function convertKelvinToCelcius(kelvinTemp) {
+exports.kelvinFunc = function () {
   if (typeof kelvinTemp !== "number" || kelvinTemp < 0)
     throw new TypeError("a kelvin temperature must be greater than 0!");
   return kelvinTemp - 273.15;
 };
-
-module.exports = function convertFahrenheitToCelcius(fahrTemp) {
+exports.fahrenheitFunc = function () {
   if ((typeof fahrTemp !== "number" || fahrTemp < -459, 67))
     throw new TypeError(
       "a fahrenheit temperature must be greater than -459.67!"
