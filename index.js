49fe25dffeaead41a246200278453adf26b1595b
unknown <darg.medhed@gmail.com>
2020-10-08 16:30:43 +0100

    version 2.2

--- index.js
+++ index.js
@@ -1,12 +1,6 @@
 exports.kelvinFunc = function (kelvinTemp) {
-  if (typeof kelvinTemp !== "number" || kelvinTemp < 0)
-    throw new TypeError("a kelvin temperature must be greater than 0!");
   return kelvinTemp - 273.15;
 };
 exports.fahrenheitFunc = function (fahrTemp) {
-  if ((typeof fahrTemp !== "number" || fahrTemp < -459, 67))
-    throw new TypeError(
-      "a fahrenheit temperature must be greater than -459.67!"
-    );
   return ((fahrTemp - 32) * 5) / 9;
 };
